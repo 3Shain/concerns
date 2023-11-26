704523b0683e5dbecec41f063f4c0b2070553a2f
3Shain <san3shain@outlook.com>
2023-11-26 09:29:27 +0000

    fix: export

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
   build: {
     lib: {
       entry: "./src/index.ts",
-      name: "concerns",
+      fileName: "index",
       formats: ["es", "cjs"],
     },
     minify: false,
